d32eb061725798d6653885926dc2f74c5a94b498
Sergii <sergii@Julias-MacBook-Pro.local>
2025-04-23 23:03:32 +0300

    task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+const listItems = [...list.children];
+
+function sortList(items) {
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  listItems.forEach((item) => list.append(item));
+}
+
+function getEmployees(items) {
+  return items.map((item) => {
+    return {
+      name: item.innerText,
+      position: item.dataset.position,
+      salary: item.dataset.salary,
+      age: item.dataset.age,
+    };
+  });
+}
+
+function parseSalary(salaryStr) {
+  const salary = parseFloat(salaryStr.replace(/[$,/s]/g, ''));
+
+  return isNaN(salary) ? 0 : salary;
+}
+
+sortList(listItems);
+getEmployees(listItems);
